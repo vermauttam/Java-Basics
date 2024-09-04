7c52bfbbd5c494a2ab55ace4dfe192c837b0eb5d
vermauttam <contacts2uttam@gmail.com>
2024-09-04 23:30:06 +0530

    Working of else if statement

---
+++ module3/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        int x = 20;
+        if (x > 10){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+    }
+}
