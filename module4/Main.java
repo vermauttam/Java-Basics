747fa6503044c4120592e8f888e359ec0c0e21e2
vermauttam <contacts2uttam@gmail.com>
2024-09-04 23:40:14 +0530

    Working of else-if else statement

---
+++ module4/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        int x =15;
+        int y =15;
+        if (x > y){
+            System.out.println(false);
+        }
+        else if (x < y) {
+            System.out.println(true);
+        }
+
+        else {
+            System.out.println("x is equal to y");
+        }
+    }
+}
