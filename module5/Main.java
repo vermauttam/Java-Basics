2e9690da59a4a791ee26919d3e48b1f164d25070
vermauttam <contacts2uttam@gmail.com>
2024-09-04 23:59:05 +0530

    Implicit/Automatic/Widening TypeCasting

---
+++ module5/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        int myInt = 15;
+        double myFloat = (double) myInt;
+        System.out.println(myFloat);
+    }
+}
