433fade3d25d6e6c3523ea836d0b9c8126ff1bb9
vermauttam <151054815+vermauttam@users.noreply.github.com>
2024-09-06 20:13:08 +0530

    Widening casting.java
    two ways to do widening casting

--- module5/Main.java
+++ module5/Main.java
@@ -3,7 +3,8 @@ public class Main
     public static void main(String args[])
     {
         int myInt = 15;
-        double myFloat = (double) myInt;
+        double myFloat = (double) myInt;  //double myFloat = (double) myInt is not required in Widening casting 
+        double myFloat = myInt;      // This will also give the same result as above
         System.out.println(myFloat);
     }
 }
