373e88b82be5ee77198ae29c3b80d05c06439cb0
vermauttam <contacts2uttam@gmail.com>
2024-09-13 00:08:18 +0530

    do-while loop

---
+++ module7/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String args[]){
+
+        int i = 5;
+
+        do{
+            System.out.println("Hello javac!");
+            i++;
+
+        }while(i<=4);
+    }
+    
+}
