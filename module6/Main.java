51d8df5b90c6497f1addf1ff771e29ab938353d5
vermauttam <contacts2uttam@gmail.com>
2024-09-12 23:56:44 +0530

    while loop

---
+++ module6/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String args[])
+    {
+        int i = 0;
+        while(i<=5){
+            System.out.println("Hello javac!");
+            i++;
+        }
+
+    
+    }
+    
+}
