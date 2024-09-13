c2fae831245e51d905276225c8401d9e602f39b2
vermauttam <contacts2uttam@gmail.com>
2024-09-13 19:10:59 +0530

    nested loop in for loop

---
+++ module9/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]){
+        for (int i = 1; i < 6; i++){
+            for (int j = 1; j < 6; j++){
+                System.out.println("Hello javac! " + i);
+            }
+        }
+    }
+    
+}
