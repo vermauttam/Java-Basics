745e71828f84d72258f5b64ac20db1c722d51636
vermauttam <contacts2uttam@gmail.com>
2024-09-26 23:32:15 +0530

    files added

---
+++ module10/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String arsg[]){
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
--- module8/Main.java
+++ module8/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String args[]){
         
-        for(int i = 0; i < 5; i++){
+        for(int i = 4; i <= 0; i--){
             System.out.println("Hello");
 
         }
