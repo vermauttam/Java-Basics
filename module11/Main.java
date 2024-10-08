5a69ae4401b1a5b9d4dcdd9885a7719496c9b7a7
vermauttam <contacts2uttam@gmail.com>
2024-10-08 23:44:46 +0530

    1-D array using Static input

---
+++ module11/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+        public static void main(String[] args){
+              int[] A = new int[5];
+
+              A[0] = 10;
+              A[1] = 20;
+              A[2] = 30;
+              A[3] = 40;
+              A[4] = 50;
+
+              for(int i=0; i<A.length; i++) {
+                System.out.println("Elements of array are: " + A[i]);
+              }
+
+        }
+}
