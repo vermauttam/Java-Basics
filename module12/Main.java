6cb34ae29bc5789c880c148f98d413fe2f7677be
vermauttam <contacts2uttam@gmail.com>
2024-10-08 23:58:38 +0530

    1-D array using user-input

---
+++ module12/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args){
+              Scanner sc = new Scanner(System.in);
+              System.out.println("Enter the size of array: ");
+              int n = sc.nextInt();
+              int[] A = new int[n];
+              System.out.println("Enter the Elements of array:");
+              for(int i=0; i<A.length; i++) {
+                A[i] = sc.nextInt();
+              }
+
+              for(int i=0; i<A.length; i++) {
+                System.out.println("Elements are: "+ A[i]);
+              }
+
+              sc.close();
+        }
+}
