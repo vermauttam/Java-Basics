2fe66ab5ab159a0dcab76e92057771f1b91c11bd
vermauttam <contacts2uttam@gmail.com>
2024-10-15 00:29:02 +0530

    Working of for loop in java

--- module8/Main.java
+++ module8/Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String args[]){
         
         for(int i = 4; i <= 0; i--){
-            System.out.println("Hello");
+            System.out.println("Hello");  
 
         }
 
