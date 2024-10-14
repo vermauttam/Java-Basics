86c5e23479a93030ac1b9051d62150480d9253fe
vermauttam <contacts2uttam@gmail.com>
2024-10-15 00:26:33 +0530

    Addition of two numbers in java

--- module10/Main.java
+++ module10/Main.java
@@ -3,6 +3,6 @@ public class Main {
         int num1 = 10;
         int num2 = 20;
         int result = num1 + num2;
-        System.out.println(result);
+        System.out.println(result);  
     }
 }
