43e0c889cec05b869770e15bf4c359dbdb0fdcef
vermauttam <contacts2uttam@gmail.com>
2024-12-24 21:17:13 +0530

    local variable initialization

---
+++ localMethod/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args){
+        String name = "Uttam";
+        nameDisplay(name);
+    }
+    public static void nameDisplay(String name){
+            System.out.println("Initialized " + name + " inside the static method");
+        }
+}
