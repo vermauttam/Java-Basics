40cad42bbd71e3742d147f51ce8d3181b3a85bbf
vermauttam <contacts2uttam@gmail.com>
2024-12-28 19:03:47 +0530

    Integer return method

---
+++ returnInteger/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static int sum(int num1, int num2){
+        return num1 + num2;
+    }
+    public static void main(String[] args){
+        int sumInteger = sum(10, 20);
+        System.out.println("Sum of two numbers is  " + sumInteger);
+    }
+}
