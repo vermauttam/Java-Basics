fee6ab52dc93e6bf05751002c041d92e6925122e
vermauttam <contacts2uttam@gmail.com>
2024-12-28 20:08:44 +0530

    Void return method

---
+++ returnVoid/Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void returnNothing(String name){
+        System.out.println("I am " + name + " Verma");
+    }
+    public static void main(String[] args){
+        returnNothing("Uttam");
+    }
+}
